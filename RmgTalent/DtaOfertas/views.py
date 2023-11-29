21bc4a5f3feea73f8c9261719a19c59743bd4409
Jean Sebastian Benitez <jean.benitez@mawesi.net>
2023-11-29 10:15:07 -0500

    Ajustes Vista

--- RmgTalent/DtaOfertas/views.py
+++ RmgTalent/DtaOfertas/views.py
@@ -1,5 +1,13 @@
+from django.http import Http404
 from django.shortcuts import render
+from models import Ofertas
 
 # Create your views here.
-# def job_listing(request):
-#     return render(request, 'mainapp/job_listing.html')
+def job_listing(request):
+    print(request)
+    try:
+        o = Ofertas.objects.get()
+    except Ofertas.DoesNotExist:
+        raise Http404("No hay ofertas")        
+    return render(request, 'mainapp/job_listing.html', {"ofertas": o})
+    
